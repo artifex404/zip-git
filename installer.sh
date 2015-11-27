155b540423cc38856bceff77cb14c269afbf0571
Igor Buyanov <igor.buyanov@monamiagency.com>
2015-11-27 14:57:54 +0200

    Change curl output flag

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sS https://raw.githubusercontent.com/artifex404/zip-git/master/zip-git.sh -O ~/zip-git.sh
+curl -sS https://raw.githubusercontent.com/artifex404/zip-git/master/zip-git.sh -o ~/zip-git.sh
 "alias zip-git='~/zip-git.sh'" >> ~/.bash_profile
 source ~/.bash_profile
 echo -e "\e[1m\e[32mZip-git installed successfully!"
