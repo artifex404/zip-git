a278e11a5766cc589bf4957566777b6193bcf000
Igor Buyanov <igor.buyanov@monamiagency.com>
2015-11-27 15:14:30 +0200

    Fix message colors

--- installer.sh
+++ installer.sh
@@ -3,4 +3,4 @@
 curl -sS https://raw.githubusercontent.com/artifex404/zip-git/master/zip-git.sh -o ~/zip-git.sh
 echo "alias zip-git='~/zip-git.sh'" >> ~/.bash_profile
 source ~/.bash_profile
-echo -e "\033[1m\033[32mZip-git installed successfully!\033[0m"
+echo -e '\033[1m\033[32mZip-git installed successfully!'
